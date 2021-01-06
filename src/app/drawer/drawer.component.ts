e3a07a26d5b3eae0fe3cb9fc47f51beff7a1be40
Melike Ercan <melike.ercan@cognite.com>
2021-01-06 18:24:36 +0100

    edited

--- src/app/drawer/drawer.component.ts
+++ src/app/drawer/drawer.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Output} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-drawer',
@@ -6,6 +6,8 @@ import {Component, OnInit, Output} from '@angular/core';
   styleUrls: ['./drawer.component.css']
 })
 export class DrawerComponent implements OnInit {
+
+  @Input() show: boolean;
 
   showFiller = false;
 
