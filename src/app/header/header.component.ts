52f323c7dabb9994307823ef576886fecf72ee82
Melike Ercan <melike.ercan.metu@gmail.com>
2021-01-11 23:50:01 +0100

    some minor changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,20 +6,15 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  isOpen: boolean;
-
   @Output() drawerClicked = new EventEmitter<boolean>();
 
   constructor() {
-    this.isOpen = false;
   }
 
   ngOnInit(): void {
   }
 
   onClick(): void {
-    this.isOpen = !this.isOpen;
-    this.drawerClicked.emit(this.isOpen);
+    this.drawerClicked.emit();
   }
 }
