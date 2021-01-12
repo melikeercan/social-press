83a1653d8d2b064f7ff0d9094e870785ee6d62cf
Melike Ercan <melike.ercan.metu@gmail.com>
2021-01-12 12:16:00 +0100

    ngswitch

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {ContentType} from '../models/content-type';
 
 interface ContentProps {
   title: string;
   description: string;
   content: string;
   tags: string[];
-  image?: string;
-  youtubeVideoId?: string;
-  instagramLink?: string;
+  sourceType: ContentType;
+  source: string;
 }
 
 @Component({
@@ -22,23 +22,20 @@ export class ContentListComponent implements OnInit {
     {
       title: 'A post shared by DogsOf (@dogsofinstagram)', description: 'Description 1', content: 'Content 1',
       tags: ['Tag11', 'Tag21', 'Tag31'],
-      image: 'https://imgur.com/gallery/vgW1p',
-      // youtubeVideoId: 'sjoWzwimUWk',
-      // instagramLink: 'http://www.instagram.com/reel/CJoe3OAAMU5/?utm_source=ig_embed&amp;utm_campaign=loading'
+      sourceType: ContentType.Image,
+      source: 'https://imgur.com/gallery/vgW1p'
     },
     {
       title: 'A post shared by Cat Lovers Club (@catloversclub)', description: 'Description 2', content: 'Content 2',
       tags: ['Tag12', 'Tag22', 'Tag32', 'Tag42'],
-      // image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-      youtubeVideoId: 'KA-DryAZanM',
-      // instagramLink: 'http://www.instagram.com/p/CIMW5hlj1md/?utm_source=ig_embed&amp;utm_campaign=loading'
+      sourceType: ContentType.Youtube,
+      source: 'KA-DryAZanM'
     },
     {
       title: 'A post shared by Food Network (@foodnetwork)', description: 'Description 3', content: 'Content 3',
       tags: ['Tag13', 'Tag23', 'Tag33', 'Tag43', 'Tag53'],
-      // image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-      // youtubeVideoId: '8v2GYW6X7WY',
-      instagramLink: 'https://www.instagram.com/tv/CJry9lrB1qT/?utm_source=ig_embed&amp;utm_campaign=loading'
+      sourceType: ContentType.Instagram,
+      source: 'https://www.instagram.com/tv/CJry9lrB1qT/?utm_source=ig_embed&amp;utm_campaign=loading'
     },
   ];
 
---
+++ src/app/models/content-type.ts
@@ -0,0 +1,1 @@
+export enum ContentType {Image = 0, Youtube = 1, Instagram = 2}
